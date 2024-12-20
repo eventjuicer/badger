c0597a37d77a4c95cb0deb83acf5a4f6364daaba
Adam Zygadlewicz <adam@zygadlewicz.com>
2024-12-20 17:53:43 +0100

    fix

--- index.cjs
+++ index.cjs
@@ -2,15 +2,15 @@
 /**
  * https://www.npmjs.com/package/node-brother-label-printer
  * sudo apt-get install build-essential libudev-dev
+ * Bus 001 Device 003: ID 04f9:2029 Brother Industries, Ltd QL-580N
 */
 
 
-const { printPngFile } = require('node-brother-label-printer');
-
+const { printPngFile } = require("node-brother-label-printer");
 
 printPngFile({
   vendorId: 0x04f9,
-  productId: 0x209d,
+  productId: 0x2029,
   filename: "./sample.png",
   options: { landscape: false, labelWidth: "62-mm-wide continuous" }, //"102-mm-wide continuous"
   compression: { enable: true },
