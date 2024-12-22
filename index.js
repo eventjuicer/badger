fb976664413221b1395869e7c75c025ad13b5212
Adam Zygadlewicz <adam@zygadlewicz.com>
2024-12-22 17:07:26 +0100

    updates

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ printPngFile({
   vendorId: 0x04f9,
   productId: 0x2029,
   filename: "./sample.png",
-  options: { landscape: false, labelWidth: "62-mm-wide continuous" }, //"102-mm-wide continuous"
+  options: { landscape: true, labelWidth: "62-mm-wide continuous" }, //"102-mm-wide continuous"
   compression: { enable: true },
 });
 
